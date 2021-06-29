7361ae4569cb40228bb05ab75874faa0fbef0114
marcelleemendess <marcellee.mendess@gmail.com>
2021-06-29 18:19:37 +0300

    First Commit

---
+++ components/BookItem.js
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+// function BookItem({title, authors, description, imageLinks}) {
+//     return (
+//         <div className="book">
+//             <h1>{title}</h1>
+//             <h3>{authors}</h3>
+//             <p>{description}{imageLinks}</p>
+//         </div>
+//     )
+// }
+
+// export default BookItem
---
+++ components/Books.js
@@ -0,0 +1,8 @@
+// import styles from '../styles/Books.module.css'
+// import { useState } from 'react';
+
+// const Books = ({books, id}) => {
+//     return books.map(book => <BookItem key={book.id}/>)
+// }
+
+// export default Books
