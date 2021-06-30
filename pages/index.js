c5f2acb0da9649eb3c50d4eaa1062ecbeb6430ba
marcelleemendess <marcellee.mendess@gmail.com>
2021-06-30 16:26:08 +0300

    add new features

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import styles from '../styles/Home.module.css'
-import Books from '../components/Books'
+
+import Head from 'next/head'
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import Spinner from '../components/layout/Spinner';
 
 
 const Home = () => {
@@ -10,39 +12,39 @@ const Home = () => {
     const [apiKey, setApiKey] = useState('AIzaSyBmaJuN9GGB2NeieGeDYbWiMY3aoOPZGvQ')
     const [loading, setLoading] = useState(false)
   
-
-    const handleChange = (e) => {
-      const book = e.target.value;
-
-      setBook(book);
-    }
-
     const handleSubmit = async (e) => {
       e.preventDefault();
-
-      await axios.get('https://www.googleapis.com/books/v1/volumes?q='+book+'&key='+apiKey+'&maxResults=40')
-      .then(data => {
-        setResult(data.data.items);
-        console.log(data.data.items);
-      })
-      .catch(error => {
-        console.log(error)
-      });
+      setLoading(true);
+      await axios
+        .get('https://www.googleapis.com/books/v1/volumes?q=' + book + '&key=' + apiKey + '&maxResults=40')
+        .then(data => {
+          setResult(data.data.items);
+          setLoading(false)
+        })
+        .catch(error => {
+        console.log(error.message)
+        });
       setBook('');
       
     }
   
-    return (
+  
+  return (
+    <>
       <div className="root">
         <div className="wrapper">
+          <Head>
+            <link real="icon" href="/favicon.ico"></link>
+          </Head>
           <h1>BookShelf</h1>
           <form onSubmit={handleSubmit} >
             <div className="search-block">
               <input
                 className="input"
                 type="text"
+                value={book}
                 placeholder="Search for Books"
-                onChange={handleChange}
+                onChange={(e) => setBook(e.target.value)}
                 required
               />
               <button
@@ -53,45 +55,54 @@ const Home = () => {
               </button>
             </div>
           </form>
-          
-          <div className="books">
-            {result.length >= 1 ? result.map(book => 
-              <div key={book.etag}className="book" >
-                    <div className="book__title">
-                      <h2>{book.volumeInfo.title}</h2>
-                    </div>
-                    <div className="book__img-block">
-                      <a  href={book.volumeInfo.previewLink} target="_blank">
-                        <img className="book__img" src={book.volumeInfo.imageLinks.thumbnail} alt={book.title} />
+
+          {loading ? (<Spinner />)
+            :
+            
+            (<div className="books">
+              {result.length >= 1 ? result.map(book => 
+                <div key={book.etag} className="book" >
+                      <div className="book__title">
+                        <h2>{book.volumeInfo.title}</h2>
+                      </div>
+                      <div className="book__img-block">
+                      <a href={book.volumeInfo.previewLink} target="_blank">
+                        {book.volumeInfo.imageLinks ?
+                            <img className="book__img" src={book.volumeInfo.imageLinks.thumbnail} alt={book.title} />
+                          :
+                            <img className="book__img" src="img/img.jpg" alt={book.title} />
+
+                        }
                       </a>
+                      </div>
+                      <div className="book__desc">
+                        <div className="book__field">
+                          <strong>Author: </strong>
+                          {book.volumeInfo.authors}
+                        </div>
+                        <div className="book__field">
+                          <strong>Published: </strong>
+                          {book.volumeInfo.publishedDate}
+                        </div>
+                        <div className="book__field">
+                          <strong>Publisher: </strong>
+                          {book.volumeInfo.publisher}
+                        </div>
+                      </div> 
                     </div>
-                    <div className="book__desc">
-                      <div className="book__field">
-                        <strong>Author: </strong>
-                        {book.volumeInfo.authors}
-                      </div>
-                      <div className="book__field">
-                        <strong>Published: </strong>
-                        {book.volumeInfo.publishedDate}
-                      </div>
-                      <div className="book__field">
-                        <strong>Publisher: </strong>
-                        {book.volumeInfo.publisher}
-                      </div>
-                    </div> 
-                  </div>
-                
-            ) : <div className="flash-info">No books to show!</div>}
-          </div>
-            <footer>
+                  
+                ) : <div className="flash-info">No books to show!</div>
+              }
+            </div>)
+          }
+          <footer>
             Made by
             <a href="https://github.com/marcelleemendess" target="_blank"> Marcelle Mendes</a>
           </footer>
-        </div>
-        
+        </div>  
       </div>
-    
-    )
+    </>
+  )
   
 }
 
